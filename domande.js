b8753df9195389bc4bd87796c28109ea4078397c
Eugenio-Pavon <eugeniopavon@yahoo.it>
2023-10-11 21:10:05 +0200

    d1_s1_m2

---
+++ domande.js
@@ -0,0 +1,5 @@
+// Un algoritmo è un processo che, attraverso processi logici e/o matematici, permette di ottenere il risultato desiderato.
+
+// Una variabile è un elemento, come ad esempio un numero o una stinga, che puo essere modificato nel tempo.
+
+// No, null significa che: il valore non é assegnato, mentre undefined significa che: non ce nessuna informazione.
